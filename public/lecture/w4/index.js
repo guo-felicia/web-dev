abe608d6cad980bf2a8711f91a025294bb61de60
tongfeiguo <tongfei.gf@gmail.com>
2022-02-16 15:52:54 -0500

    wk4 jQuery

---
+++ public/lecture/w4/index.js
@@ -0,0 +1,12 @@
+alert('hello from javascript')
+
+// function init(){
+//     alert('hello from javascript');
+//     jQuery('h1').remove);
+//     $('h1'.css('color','red'));
+//
+//     const heading1 = $('h1');
+//     heading1.css('color','yellow').css('background-color','purple');
+// }
+//
+// jQuery(init)
