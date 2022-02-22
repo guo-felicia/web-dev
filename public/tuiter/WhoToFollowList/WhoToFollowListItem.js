d87543358fedec7a80990a2442a98ab936bc348d
tongfeiguo <tongfei.gf@gmail.com>
2022-02-22 16:28:17 -0500

    who list

---
+++ public/tuiter/WhoToFollowList/WhoToFollowListItem.js
@@ -0,0 +1,25 @@
+const WhoToFollowListItem = (who) => {
+    return(`
+        <li>
+
+            <div class="wd-container-card">
+                <div class="wd-left-avatar">
+                    <img class="wd-avatar" src="${who.avatarIcon}" alt="Avatar">
+                </div>
+                <div class="wd-left-element">
+                    <div class="wd-title-block d-flex align-items-center">
+                        <p class="wd-title-15px m-0">${who.userName}</p>
+                        <i class="fas fa-check-circle wd-icon"></i>
+                    </div>
+                    <div class="wd-label-15px">@${who.handle}</div>
+
+                </div>
+                <div class="wd-right-element">
+                    <button class="btn btn-primary override-bs">
+                        Follow
+                    </button>
+                </div>
+            </div>
+        </li>`);
+}
+export default WhoToFollowListItem;
