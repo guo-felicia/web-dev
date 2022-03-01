95089d0e65fa0e1fcc652ca3594be594756767b5
tongfeiguo <tongfei.gf@gmail.com>
2022-02-28 21:05:22 -0500

    a6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
+import './vendors/bootstrap/css/bootstrap.min.css';
+import './vendors/bootstrap/bootstrap.min.css';
+import './vendors/fontawesome/css/all.min.css';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Web Dev!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <h1>Hello World!</h1>
+        </div>
+    );
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Welcome to Web Dev!
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -3,16 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import TuitApp from "./components/TuitApp";
-import Login from "./components/Login";
-import Register from "./components/Register";
+import TuitApp from "./components/wk6_lec/TuitApp";
+import Login from "./components/wk6_lec/Login";
+import Register from "./components/wk6_lec/Register";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <TuitApp/>
-    </React.StrictMode>,
+    <App />,
     document.getElementById('root')
 );
+
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <TuitApp/>
+//     </React.StrictMode>,
+//     document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
