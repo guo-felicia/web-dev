d774df53c8abbda6706276c8fd29239d3bd3419d
tongfeiguo <tongfei.gf@gmail.com>
2022-03-03 10:01:39 -0500

    a6 lab

---
+++ src/components/Tuiter/WhoToFollowList/WhoToFollowListItem.js
@@ -0,0 +1,35 @@
+// 1. Import React.js
+import React from "react";
+
+const WhoToFollowListItem = (
+    // 2. Object deconstructed parameter
+    {
+        who = {
+            avatarIcon: '../../../../public/tuiter/image/avatar3.jpg',
+            userName: 'NASA',
+            handle: 'NASA',
+        }
+    }
+) => {
+    return (
+            <div className="wd-container-card">
+                <div className="wd-left-avatar">
+                    <img className="wd-avatar" src={who.avatarIcon} alt="Avatar"/>
+                </div>
+                <div className="wd-left-element">
+                    <div className="wd-title-block d-flex align-items-center">
+                        <p className="wd-title-15px m-0">{who.userName}</p>
+                        <i className="fas fa-check-circle wd-icon"></i>
+                    </div>
+                    <div className="wd-label-15px">@{who.handle}</div>
+                
+                </div>
+                <div className="wd-right-element">
+                    <button className="btn btn-primary override-bs">
+                        Follow
+                    </button>
+                </div>
+            </div>
+);
+}
+export default WhoToFollowListItem;
--- src/components/Tuiter/index.js
+++ src/components/Tuiter/index.js
@@ -1,18 +1,32 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import NavigationSidebar from "./NavigationSidebar/index"
+import WhoToFollowListItem from "./WhoToFollowList/WhoToFollowListItem";
 
 const Tuiter = () => {
-    return(
+    return (
         <>
-            <h1>Tuiter</h1>
-            <Link to="/hello">
-                Hello
-            </Link> |
-            <Link to="/labs">
-                Labs
-            </Link>
+            <NavigationSidebar active="home"/>
+            <WhoToFollowListItem/>
         </>
     )
 };
-
 export default Tuiter;
+
+
+// Practice of Labs
+// const Tuiter = () => {
+//     return(
+//         <>
+//             <h1>Tuiter</h1>
+//             <NavigationSidebar/>
+//             <Link to="/hello">
+//                 Hello
+//             </Link> |
+//             <Link to="/labs">
+//                 Labs
+//             </Link>
+//         </>
+//     )
+// };
+// export default Tuiter;
