7d3f29dbd386c287e5049b6791ad3633ec79ab92
tongfeiguo <tongfei.gf@gmail.com>
2022-03-04 11:34:19 -0500

    a5 fix WhoToFollow

--- public/tuiter/WhoToFollowList/index.js
+++ public/tuiter/WhoToFollowList/index.js
@@ -1,15 +1,16 @@
 import WhoToFollowListItem from "./WhoToFollowListItem.js";
 import who from "./who.js";
+// import "../explore.css"
 
 const WhoToFollowList = () => {
     return (`
             <ul class="list-group">
-            <div className="list-group-item wd-bold">Who to Follow</div>
+            <div class="list-group-item wd-bold">Who to Follow</div>
             ${
-                who.map(whoItem => {
-                    return(WhoToFollowListItem(whoItem));
-                }).join('')
-            }
+        who.map(whoItem => {
+            return (WhoToFollowListItem(whoItem));
+        }).join('')
+    }
             </ul>
     `);
 }
